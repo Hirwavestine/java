cbcc030ef981abcdec95a1c7d36fc49077c5aaab
hirwavestine <gilhirwa22@gmail.com>
2019-05-28 20:49:06 +0200

    updating an element of an array

---
+++ array2.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+      // Assign a list of names to the names variable
+      String[] names={"Ken","Master White","Ben"};
+      
+      // Print the element at index 0
+      System.out.println(names[0]);
+      names[0]="Williams";
+      System.out.println(names[0]);
+      
+    }
+  }
